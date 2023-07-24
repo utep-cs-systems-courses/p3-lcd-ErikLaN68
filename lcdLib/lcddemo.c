d17f792ab4a145831e36547be770b3d17e4b3313
ErikLaN68 <eriklaneave@gmail.com>
2023-07-23 20:06:08 -0600

    changes from class

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -6,6 +6,26 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
+int max(int a, int b) {return a > b ? a : b; }
+int min(int a, int b) {return a > b ? b : a; }
+
+void drawHourGlass(int controlCol, int controlRow, int size, short color)
+{
+  int c = 0;
+  int r = 0;
+  for (int j = 0; j < size; j++) {
+    int upper = min(-r, +r);
+    int lower = max(-r, +r);
+    int drawCol = c;
+    for (int i = upper; i <= lower; i++) {
+      drawPixel(controlCol+drawCol,controlRow+i,color);
+      drawPixel(controlCol-drawCol,controlRow+i,color);
+    }
+    c += 1;
+    r += 1;
+  }
+}
+
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
 main()
@@ -14,10 +34,30 @@ main()
   lcd_init();
   u_char width = screenWidth, height = screenHeight;
 
-  clearScreen(COLOR_BLUE);
+  clearScreen(COLOR_BLACK);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  //drawString5x7(20,20, "Erik LaNeave", COLOR_GREEN, COLOR_BLUE);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  //fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  
+  drawHourGlass(screenWidth >> 1, screenHeight >> 1, 15, COLOR_WHITE);
   
 }
+
+/*
+void drawHourGlass(int controlCol, int ControlRow, int size, u_int16 color)
+{
+  int c = 0;
+  int r = 0;
+  for (int j = 0; j < size; j++) {
+    int upper = min(-r, +r);
+    int lower = max(-r, +r);
+    int drawCol = c;
+    for (int i = upper; i <= lower; i++) {
+      drawPixel(controlCol+drawCol,controlRow+i,color);
+      drawPixel(controlCol-drawCol,controlRow+i,color);
+    }
+    c += 1;
+    r += 1;
+  }
+}*/
