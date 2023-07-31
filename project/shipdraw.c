fdd8a2216879f4f71848b9cbd1f4aa33b7b02d6b
ErikLaN68 <eriklaneave@gmail.com>
2023-07-30 18:17:03 -0600

    Have a triangle that moves across the bottom of the screen with buttons 1 and 4

---
+++ project/shipdraw.c
@@ -0,0 +1,25 @@
+#include "lcdutils.h"
+#include "lcddraw.h"
+
+void
+draw_ship(int controlCol, int controlRow, short color)
+{
+  //fixed car size
+  char size = 21;
+  //Runs the for loop the correct amount of times
+  char stop = (size/2) + 1;
+  char count = 0;
+  //How many layers are made
+  while (count < stop) {
+    //Runs till size to make the layers
+    for (int i = 0; i < size; i++) {
+      //only have to add to controlCol to move along
+      drawPixel(controlCol+i,controlRow,color);
+    }
+    //moving the position up and decreasing the size
+    size -= 2;
+    controlCol += 1;
+    controlRow -= 1;
+    count++;
+  }
+}
---
+++ project/shipdraw.h
@@ -0,0 +1,6 @@
+#ifndef cardraw_included
+#define lcdtypes_included
+
+void draw_ship(int controlCol, int controlRow, short color);
+
+#endif
